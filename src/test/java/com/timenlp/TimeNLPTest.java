dfbf5e02e919996ed90ec4fed3e472bccdb86cfa
Bernard897 <Bernard@funai.email>
2025-04-24 08:53:59 +0000

    Issue #17: Combined Date and Time Parsing

--- src/test/java/com/timenlp/TimeNLPTest.java
+++ src/test/java/com/timenlp/TimeNLPTest.java
@@ -1,12 +1,36 @@
 package com.timenlp;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import com.timenlp.entity.DateEntity;
+import org.junit.jupiter.api.Test;
 
-public class TimeNLPTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeNLPTest {
 
     @Test
-    public void testApp() {
-        assertTrue(true);
+    void parseDateTime() {
+        TimeNLP timeNLP = new TimeNLP();
+        DateEntity dateEntity = timeNLP.parseDateTime("2023-10-27 10:00");
+        assertNotNull(dateEntity);
+        assertEquals(2023, dateEntity.getYear());
+        assertEquals(10, dateEntity.getMonth());
+        assertEquals(27, dateEntity.getDay());
+        assertEquals(10, dateEntity.getHour());
+        assertEquals(0, dateEntity.getMinute());
+
+        dateEntity = timeNLP.parseDateTime("明天 10:00");
+        assertNotNull(dateEntity);
+        assertEquals(10, dateEntity.getHour());
+        assertEquals(0, dateEntity.getMinute());
+
+        dateEntity = timeNLP.parseDateTime("2024-02-29 10:00");
+        assertNotNull(dateEntity);
+        assertEquals(2024, dateEntity.getYear());
+        assertEquals(2, dateEntity.getMonth());
+        assertEquals(29, dateEntity.getDay());
+        assertEquals(10, dateEntity.getHour());
+        assertEquals(0, dateEntity.getMinute());
+
+
     }
 }
